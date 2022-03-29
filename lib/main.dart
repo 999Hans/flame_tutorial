16a4f8c19275a5c39042045de87dbe9a0c49f373
999Hans <ohy0720@gmail.com>
2022-03-29 21:26:31 +0900

    add background, add motion of girl and boy, add flipHorizontal

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,55 @@ void main() {
 class MyGame extends FlameGame {
   SpriteComponent girl = SpriteComponent();
   SpriteComponent boy = SpriteComponent();
+  SpriteComponent background = SpriteComponent();
 
   final double characterSize = 200;
+  final textBoxSize = 100;
 
+  bool turnAway = false;
     @override
     Future<void> onLoad() async{
          super.onLoad();
          final screenWidth = size[0];
          final screenHeight = size[1];
-         print(size);
-         print(screenWidth);
-         print(screenHeight);
+
+         //load backGround
+      add(background
+          ..sprite = await loadSprite('background.png')
+          ..size = size
+      );
 
          //load load girl, boy image
       girl
          ..sprite = await loadSprite('girl.png')
          ..size = Vector2(characterSize, characterSize)
+         ..anchor = Anchor.topCenter
          ..x = 0
-         ..y = screenHeight - characterSize - 100;
+         ..y = screenHeight - characterSize - textBoxSize;
       add(girl);
       boy
          ..sprite = await loadSprite('boy.png')
          ..size = Vector2(characterSize, characterSize)
+        ..anchor = Anchor.topCenter
+        ..flipHorizontally()
          ..x = screenWidth - characterSize
-         ..y = screenHeight - characterSize - 100 ;
+         ..y = screenHeight - characterSize - textBoxSize ;
       add(boy);
+    }
+
+    @override
+  void update(double dt){
+      super.update(dt);
+      if (girl.x < size[0] / 2 -150){
+        girl.x += 30 * dt;
+      }
+      else if (turnAway == false){
+        boy.flipHorizontally();
+        girl.flipHorizontally();
+        turnAway = true;
+      }
+
+      if (boy.x > size[0] / 2){
+        boy.x -= 30 * dt; }
     }
 }
